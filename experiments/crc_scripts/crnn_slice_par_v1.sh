a737ed7a018cee25157389efd614b287dd08ecc0
prijatelj <theomnipotentwoof@gmail.com>
2020-10-02 00:55:28 -0400

    updated crc script

--- experiments/crc_scripts/crnn_slice_par_v1.sh
+++ experiments/crc_scripts/crnn_slice_par_v1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#$ -pe smp 16
+#$ -pe smp 8
 #$ -N crnn_parv1_slice
 #$ -q gpu
 #$ -l gpu=1
-#$ -o $HOME/scratch_22/open_set/hwr/hwr_novelty/logs/crnnEvm/logs/
-#$ -e $HOME/scratch_22/open_set/hwr/hwr_novelty/logs/crnnEvm/logs/
+#$ -o $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/crnn/par/slice/logs/
+#$ -e $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/crnn/par/slice/logs/
 
 BASE_PATH="$HOME/scratch_365/open_set/hwr/hwr_novelty/"
 #DATA_PATH="$HOME/scratch_22/open_set/data/image_net"
@@ -14,7 +14,7 @@ BASE_PATH="$HOME/scratch_365/open_set/hwr/hwr_novelty/"
 module add conda
 conda activate osr_torch
 
-python3 "$BASE_PATH/experiments/research/par_v1/crnn_scripts.py" \
+python3 "$BASE_PATH/experiments/research/par_v1/crnn_script.py" \
     "$BASE_PATH/experiments/configs/par_iam_v1/crnn/par_iam_v1_crnn_slice.yaml" \
     --slice "rnn" \
     --eval "train" "test"
