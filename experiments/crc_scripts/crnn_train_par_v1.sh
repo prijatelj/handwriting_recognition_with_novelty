107202d0f72ccdb2a9030918426f21123d021129
prijatelj <theomnipotentwoof@gmail.com>
2020-10-02 00:53:48 -0400

    updated train script

--- experiments/crc_scripts/crnn_train_par_v1.sh
+++ experiments/crc_scripts/crnn_train_par_v1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#$ -pe smp 16
+#$ -pe smp 8
 #$ -N crnn_parv1
 #$ -q gpu
 #$ -l gpu=1
-#$ -o $HOME/scratch_22/open_set/hwr/hwr_novelty/logs/crnnEvm/logs/
-#$ -e $HOME/scratch_22/open_set/hwr/hwr_novelty/logs/crnnEvm/logs/
+#$ -o $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/crnn/par/train/logs/
+#$ -e $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/crnn/par/train/logs/
 
-BASE_PATH="$HOME/scratch_365/open_set/hwr/hwr_novelty/"
+BASE_PATH="$HOME/scratch_365/open_set/hwr/hwr_novelty"
 #DATA_PATH="$HOME/scratch_22/open_set/data/image_net"
 
 # set up the environment
 module add conda
 conda activate osr_torch
 
-python3 "$BASE_PATH/experiments/research/par_v1/crnn_scripts.py" \
+python3 "$BASE_PATH/experiments/research/par_v1/crnn_script.py" \
     "$BASE_PATH/experiments/configs/par_iam_v1/crnn/par_iam_v1_crnn_train.yaml"
