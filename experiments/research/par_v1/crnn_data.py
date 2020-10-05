8feb432467e94015009fbbfc2d429d8e02ac0a0e
prijatelj <theomnipotentwoof@gmail.com>
2020-10-05 11:47:02 -0400

    added TranscriptResults datasclass and redid old_load_dataset to match expected config pattern.

--- experiments/research/par_v1/crnn_data.py
+++ experiments/research/par_v1/crnn_data.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 import logging
 
 # 3rd party packages
+import numpy as np
 import torch
 from torch.utils.data import DataLoader, Subset
 
@@ -28,49 +29,91 @@ class DataSet:
     test_dataset: hw_dataset.HwDataset
     test_dataloader: DataLoader
 
-def load_data(data_config):
+
+@dataclass
+class TranscriptResults:
+    """Contains everything for the dataset handling in one place."""
+    char_errors: int
+    word_errors: int
+    char_confusion_mat: np.ndarray
+
+
+def load_datasplit(config, img_hieight=64):
     """Loads all datasets contained within the given data config.
 
     Parameters
     ----------
 
     """
-    return
-
-def old_load_dataset(config, dataset, RIMES=False, always_val=False):
-    """Loads the datasets from the given dataset config."""
-    idx_to_char, char_to_idx = character_set.load_label_set(
-        config[dataset]['labels'],
-    )
-
-    train_dataset = hw_dataset.HwDataset(
-        config[dataset]['training_set_path'],
+    raise NotImplementedError('use old load deataset instead.')
+    dataset = hw_dataset.HwDataset(
+        config[dataset_id]['train'],
         char_to_idx,
         img_height=config['network']['input_height'],
         root_path=config[dataset]['image_root_directory'],
         augmentation=False,
     )
 
-    train_dataloader = DataLoader(
-        train_dataset,
+    dataloader = DataLoader(
+        dataset,
         batch_size=config['batch_size'],
         shuffle=False,
         num_workers=1,
         collate_fn=hw_dataset.collate,
     )
+    return
 
-    try:
-        val_dataset = hw_dataset.HwDataset(
-            config[dataset]['validation_set_path'],
+def old_load_dataset(config, dataset, shuffle=False, always_val=False):
+    """Loads the datasets from the given dataset config."""
+    idx_to_char, char_to_idx = character_set.load_label_set(
+        config[dataset]['labels'],
+    )
+
+    data_exists = False
+
+    if 'train' in config['data'][dataset]:
+        train_dataset = hw_dataset.HwDataset(
+            config['data'][dataset]['training_set_path'],
             char_to_idx,
             img_height=config['network']['input_height'],
-            root_path=config[dataset]['image_root_directory'],
+            root_path=config['data'][dataset]['image_root_directory'],
+            augmentation=False,
+        )
+
+        train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=config['batch_size'],
+            shuffle=shuffle,
+            num_workers=1,
+            collate_fn=hw_dataset.collate,
+        )
+        data_exists = True
+    else:
+        train_dataset = None
+        train_dataloader = None
+
+    if 'val' in config['data'][dataset]:
+        val_dataset = hw_dataset.HwDataset(
+            config['data'][dataset]['val'],
+            char_to_idx,
+            img_height=config['network']['input_height'],
+            root_path=config['data'][dataset]['image_root_directory'],
+            #augmentation=False,
             remove_errors=True,
         )
 
-    except KeyError as e:
-        if always_val:
-            print("No validation set found, generating one")
+        val_dataloader = DataLoader(
+            val_dataset,
+            batch_size=config['batch_size'],
+            shuffle=shuffle,
+            num_workers=1,
+            collate_fn=hw_dataset.collate,
+        )
+
+        data_exists = True
+    elif always_val:
+        print("No validation set found, generating one")
+        if data_exists:
             master = train_dataset
 
             print("Total of " +str(len(master)) +" Training Examples")
@@ -87,34 +130,44 @@ def old_load_dataset(config, dataset, RIMES=False, always_val=False):
             val_dataloader = DataLoader(
                 val_dataset,
                 batch_size=config['batch_size'],
-                shuffle=False,
+                shuffle=shuffle,
                 num_workers=1,
                 collate_fn=hw_dataset.collate,
             )
         else:
-            val_dataset = None
-            val_dataloader = None
+            raise KeyError('No training set exists, but `always_val` is True')
+    else:
+        val_dataset = None
+        val_dataloader = None
 
-    test_dataset = hw_dataset.HwDataset(
-        config[dataset]['test_set_path'],
-        char_to_idx,
-        img_height=config['network']['input_height'],
-        root_path=config[dataset]['image_root_directory'],
-        remove_errors=True,
-    )
-    test_dataloader = DataLoader(
-        test_dataset,
-        batch_size=config['batch_size'],
-        shuffle=False,
-        num_workers=0,
-        collate_fn=hw_dataset.collate,
-    )
+    if 'test' in config['data'][dataset]:
+        test_dataset = hw_dataset.HwDataset(
+            config['data'][dataset]['test'],
+            char_to_idx,
+            img_height=config['network']['input_height'],
+            root_path=config['data'][dataset]['image_root_directory'],
+            remove_errors=True,
+        )
+        test_dataloader = DataLoader(
+            test_dataset,
+            batch_size=config['batch_size'],
+            shuffle=shuffle,
+            num_workers=0,
+            collate_fn=hw_dataset.collate,
+        )
+        data_exists = True
+    else:
+        test_dataset = None
+        test_dataloader = None
+
+    if not data_exists:
+        raise KeyError('No train, val, or test set given in config!')
 
     return DataSet(
         idx_to_char,
         char_to_idx,
         train_dataset,
-        train_dataloader, # TODO add this, be aware of this
+        train_dataloader,
         val_dataset,
         val_dataloader,
         test_dataset,
