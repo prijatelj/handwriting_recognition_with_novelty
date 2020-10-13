89c8c953279b4153d3ef10e8c820508d0195dc65
prijatelj <theomnipotentwoof@gmail.com>
2020-10-13 18:43:03 -0400

    made space_char an arg and added debug logging

--- experiments/research/par_v1/grieggs/error_rates.py
+++ experiments/research/par_v1/grieggs/error_rates.py
@@ -1,10 +1,15 @@
+import logging
+
 import editdistance
 
 
-def cer(r, h):
+def cer(r, h, space_char=u' '):
     #Remove any double or trailing
-    r = u' '.join(r.split())
-    h = u' '.join(h.split())
+    r = space_char.join(r.split())
+    h = space_char.join(h.split())
+
+    logging.debug('cer: r = `%s`; h = `%s`', r, h)
+
     return err(r, h)
 
 def err(r, h):
