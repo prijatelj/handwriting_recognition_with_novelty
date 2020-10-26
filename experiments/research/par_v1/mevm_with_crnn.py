7d98c2effd4b22cd8a1a453a13fa982176b3256d
prijatelj <theomnipotentwoof@gmail.com>
2020-10-25 20:44:01 -0400

    added duplication of last element to pad layer_out and col_chars to correct length.

--- experiments/research/par_v1/mevm_with_crnn.py
+++ experiments/research/par_v1/mevm_with_crnn.py
@@ -152,6 +152,26 @@ def col_chars_crnn(crnn, dataloader, char_enc, dtype, layer='rnn', repeat=4):
     for i in range(len(layer_out)):
         layer_out[i] = np.repeat(layer_out[i], repeat, axis=0)
 
+        # TODO this is a hotfix and needs replaced eventually. May cause errors
+        if layer_out[i].shape[0] > col_chars[i].shape[0]:
+            # Duplicates the last charater to pad the layer to the size of the
+            # layer_out
+            col_chars[i] = np.append(
+                col_chars[i],
+                col_chars[i][[-1] * layer_out[i].shape[0]
+                    - col_chars[i].shape[0]],
+            )
+
+        elif layer_out[i].shape[0] < col_chars[i].shape[0]:
+            # Duplicates the last layer to pad the layer to the size of the
+            # pixel columns
+            layer_out[i] = np.append(
+                layer_out[i],
+                layer_out[i][[-1] * col_chars[i].shape[0]
+                    - layer_out[i].shape[0]],
+                axis=0,
+            )
+
     layer_out_conc = np.concatenate(layer_out)
     col_chars_conc = np.concatenate(col_chars)
 
