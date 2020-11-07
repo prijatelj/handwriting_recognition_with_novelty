4906d984d052609496b43d08ce86e5e9c4da2a94
prijatelj <theomnipotentwoof@gmail.com>
2020-11-06 21:10:35 -0500

    corrected abstractmethod use

--- hwr_novelty/models/predictor.py
+++ hwr_novelty/models/predictor.py
@@ -81,7 +81,85 @@ class SupervisedTranscripter(SupervisedClassifier):
         self.char_enc = CharEncoder(*args, **kwargs)
         self.label_enc = self.char_enc
 
-    @asbtractmethod
+    @abstractmethod
+    def fit(self, features, labels):
+        """Given the current state of the predictor, continue training given
+        the provided data. This uses the existing state of the predictor.
+        """
+
+        # TODO fit in batches
+        # TODO fit incrementally
+        # TODO parameterize init, fit, and predict st they may be provided args
+        # & kwargs as configs
+        pass
+
+
+class SupervisedClassifier(SupervisedLearner):
+    """Abstract class for supervised learning classifiers.
+
+    Attributes
+    ----------
+    label_enc : NominalDataEncoder
+    """
+
+    # TODO __init__ either sets random state or setups up random state to be
+    # created when first fit occurs (implies unable to predict in this case).
+
+    def __init__(self, *args, **kwargs):
+        self.label_enc = NominalDataEncoder(*args, **kwargs)
+
+
+class SupervisedTranscripter(SupervisedClassifier):
+    """Abstract class for supervised learning predictors.
+
+    Attributes
+    ----------
+    char_enc : CharacterEncoder
+    """
+
+    # TODO __init__ either sets random state or setups up random state to be
+    # created when first fit occurs (implies unable to predict in this case).
+
+    def __init__(self, *args, **kwargs):
+        self.char_enc = CharEncoder(*args, **kwargs)
+        self.label_enc = self.char_enc
+
+    @abstractmethod
+    def fit(self, features, labels):
+        """Given the current state of the predictor, continue training given
+        the provided data. This uses the existing state of the predictor.
+        """
+        # TODO fit in batches
+        # TODO fit incrementally
+        # TODO parameterize init, fit, and predict st they may be provided args
+        # & kwargs as configs
+        pass
+
+
+# TODO MEVM(MultipleMEVM, SupervisedLearner):
+#   fit(), predict(), NominalDataEncoder
+#   save updated to include a dataset for the labels for NominalDataEncoder
+
+#class ANNMEVM(SupervisedTranscripter)
+#    """The combination of an artificial neural network and an MEVM.
+#
+#    Attributes
+#    ----------
+#    ann
+#    mevm : MEVM
+#        The MEVM class that wraps the MultipleEVM class.
+#    """
+#
+#    def __init__(self):
+#        pass
+#
+#
+#class CRNNMEVM(ANNMEVM):
+#    """The CRNN and MEVM combined to form a transcripter."""
+#
+#class MEVMBasedHWRDetector():
+#
+#class MEVMBasedHWRAdapter():
     def fit(self, features, labels):
         """Given the current state of the predictor, continue training given
         the provided data. This uses the existing state of the predictor.
