2e62c2a28c7f8d807c77a6c7a43abedfa0989922
prijatelj <theomnipotentwoof@gmail.com>
2020-12-24 11:00:01 -0500

    initial

---
+++ hwr_novelty/generate/background.py
@@ -0,0 +1,8 @@
+"""Generates a background or changes a given image to apply the background
+type.
+"""
+
+# TODO have a general
+
+# TODO get mutually exclusive repr labels:
+#   binary/grayscale, noisy, antique, blur
