454f5e66c9d207f183356300e1305792dc939d73
prijatelj <theomnipotentwoof@gmail.com>
2021-01-10 03:10:32 -0500

    corrected typo

--- experiments/data/iam.py
+++ experiments/data/iam.py
@@ -173,7 +173,7 @@ class HWR(object):
             raise IOError(f'`image` is None. image path: {img_path}')
         elif not np.isfinite(image).all():
             raise ValueError(f'`image` is not finte. image path: {img_path}')
-        elif np.isna(image).any():
+        elif np.isnan(image).any():
             raise ValueError(f'`image` contains NA. image path: {img_path}')
 
         # Resize image based on given image height
