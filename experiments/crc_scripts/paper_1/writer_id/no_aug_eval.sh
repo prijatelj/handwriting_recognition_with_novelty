e485f5abbc990bf4d7a0a51e8411e828e4bdc6f7
prijatelj <theomnipotentwoof@gmail.com>
2021-01-14 20:21:28 -0500

    update

--- experiments/crc_scripts/paper_1/writer_id/no_aug_eval.sh
+++ experiments/crc_scripts/paper_1/writer_id/no_aug_eval.sh
@@ -6,7 +6,7 @@
 #$ -l gpu=1
 #$ -o $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/paper/mevm/eval/logs/
 #$ -e $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/paper/mevm/eval/logs/
-#$ -t 6-23
+#$ -t 12-23
 
 BASE_PATH="$HOME/scratch_365/open_set/hwr/hwr_novelty"
 
@@ -15,7 +15,7 @@ BASE_SPLIT="/afs/crc.nd.edu/user/d/dprijate/scratch_22/open_set/data/handwritten
 BASE_IAM="$BASE_SPLIT/paper_1/data/iam_splits"
 BASE_RIMES="$BASE_SPLIT/paper_1/data/rimes_splits"
 
-BASE_OUT="$BASE_SPLIT/paper_1/writer_id/mevm"
+BASE_OUT="$BASE_SPLIT/paper_1/writer_id/mevm/hogs"
 
 # set up the environment
 module add conda
@@ -34,8 +34,9 @@ MODO=$(($SGE_TASK_ID % 6))
 if [ "$MODO" -eq "5" ]; then
     IAM_PATH="$BASE_SPLIT/grieggs_data/IAM_aachen/train.json"
     RIMES_PATH="$BASE_SPLIT/grieggs_data/RIMES_2011_LINES/training_2011_gt.json"
-    OUT_PATH="$BASE_OUT/bfaithful/mevm_bfaithful_writer_id_no_aug_eval"
-    MEVM_LOAD="$BASE_OUT/bfaithful/mevm_bfaithful_writer_id_no_aug.hdf5"
+    OUT_PATH="$BASE_OUT/bfaithful/mevm_bfaithful_writer_id_no_aug"
+    MEVM="$OUT_PATH".hdf5
+    OUT_PATH="$OUT_PATH"_eval
 
     python3 "$BASE_PATH/experiments/research/mevm_style.py" \
         "$BASE_PATH/experiments/configs/paper_1/mevm_writer_id_no_aug.yaml" \
@@ -44,14 +45,16 @@ if [ "$MODO" -eq "5" ]; then
         --iam_image_root_dir "$BASE_SPLIT/grieggs_data/IAM_aachen/" \
         --rimes_path "$RIMES_PATH" \
         --output_path "$OUT_PATH"/"$DATASPLIT".csv \
-        --mevm_load "$MEVM_LOAD" \
+        --output_points "$OUT_PATH"/"$DATASPLIT"_points.csv \
+        --mevm_load "$MEVM" \
         --datasplit "$DATASPLIT"
     exit 0
 else
     IAM_PATH="$BASE_IAM/"iam_split_"$MODO"_labels.json
     RIMES_PATH="$BASE_RIMES/"rimes_split_"$MODO".json
-    OUT_PATH="$BASE_OUT/"split_"$MODO"/mevm_split_"$MODO"_writer_id_no_aug_eval
-    MEVM_LOAD="$BASE_OUT/"split_"$MODO"/mevm_split_"$MODO"_writer_id_no_aug.hdf5
+    OUT_PATH="$BASE_OUT/"split_"$MODO"/mevm_split_"$MODO"_writer_id_no_aug
+    MEVM="$OUT_PATH".hdf5
+    OUT_PATH="$OUT_PATH"_eval
 fi
 
 python3 "$BASE_PATH/experiments/research/mevm_style.py" \
@@ -60,5 +63,6 @@ python3 "$BASE_PATH/experiments/research/mevm_style.py" \
     --iam_path "$IAM_PATH" \
     --rimes_path "$RIMES_PATH" \
     --output_path "$OUT_PATH"/"$DATASPLIT".csv \
-    --mevm_load "$MEVM_LOAD" \
+    --output_points "$OUT_PATH"/"$DATASPLIT"_points.csv \
+    --mevm_load "$MEVM" \
     --datasplit "$DATASPLIT"
