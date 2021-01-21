93148cfe19cca1430f1721dd1f86457eeb79b0ad
prijatelj <theomnipotentwoof@gmail.com>
2021-01-20 22:24:31 -0500

    adding to not further muddy my tracks

---
+++ experiments/crc_scripts/paper_1/writer_id/crnn_pca_train.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#$ -pe smp 8
+#$ -N evmWC
+#$ -q gpu@@cvrl
+#$ -l gpu=1
+#$ -o $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/paper/mevm/train/logs/
+#$ -e $HOME/scratch_365/open_set/hwr/hwr_novelty/logs/paper/mevm/train/logs/
+#$ -t 18-21
+
+BASE_PATH="$HOME/scratch_365/open_set/hwr/hwr_novelty"
+
+BASE_SPLIT="/afs/crc.nd.edu/user/d/dprijate/scratch_22/open_set/data/handwritten_text_recognition"
+
+BASE_IAM="$BASE_SPLIT/paper_1/data/iam_splits"
+BASE_RIMES="$BASE_SPLIT/paper_1/data/rimes_splits"
+
+BASE_OUT="/afs/crc.nd.edu/user/d/dprijate/scratch_21/open_set/hwr_novelty"
+BASE_OUT="$BASE_OUT/paper_1/writer_id/mevm/crnn/pca"
+
+if [ "$SGE_TASK_ID" -lt "12" ]; then
+    DATASPLIT="train"
+elif [ "$SGE_TASK_ID" -lt "18" ]; then
+    DATASPLIT="val"
+else
+    DATASPLIT="test"
+fi
+
+# set up the environment
+module add conda
+conda activate osr_torch
+
+# Unique path addons per run
+MODO=$(($SGE_TASK_ID % 6))
+if [ "$MODO" -eq "5" ]; then
+    echo "None of that."
+    exit 0
+    IAM_PATH="$BASE_SPLIT/grieggs_data/IAM_aachen/train.json"
+    RIMES_PATH="$BASE_SPLIT/grieggs_data/RIMES_2011_LINES/training_2011_gt.json"
+    OUT_PATH="$BASE_OUT/bfaithful/mevm_bfaithful_writer_id_no_aug"
+    MEVM="$OUT_PATH".hdf5
+
+    python3 "$BASE_PATH/experiments/research/mevm_style.py" \
+        "$BASE_PATH/experiments/configs/paper_1/mevm_writer_id_no_aug.yaml" \
+        --log_level INFO \
+        --iam_path "$IAM_PATH" \
+        --iam_image_root_dir "$BASE_SPLIT/grieggs_data/IAM_aachen/" \
+        --rimes_path "$RIMES_PATH" \
+        --output_path "$OUT_PATH".csv \
+        --mevm_save "$MEVM"
+    exit 0
+else
+    IAM_PATH="$BASE_IAM/"iam_split_"$MODO"_labels.json
+    RIMES_PATH="$BASE_RIMES/"rimes_split_"$MODO".json
+    OUT_PATH="$BASE_OUT/"split_"$MODO"/split_"$MODO"_crnn_pca
+    MEVM="$OUT_PATH".hdf5
+
+    EMB_FP="$OUT_PATH/$DATASPLIT/config$MODO"_"$DATASPLIT"_embeddings_PCA_1000_points.hdf5
+    OUT_PATH="$OUT_PATH"_eval
+fi
+
+if [ "$SGE_TASK_ID" -lt "12" ]; then
+    python3 "$BASE_PATH/experiments/research/mevm_style.py" \
+        "$BASE_PATH/experiments/configs/paper_1/mevm_writer_id_no_aug.yaml" \
+        --log_level INFO \
+        --iam_path "$IAM_PATH" \
+        --rimes_path "$RIMES_PATH" \
+        --output_path "$OUT_PATH"/"$DATASPLIT".csv \
+        --mevm_save "$MEVM" \
+        --datasplit "$DATASPLIT" \
+        --embed_filepath "$EMB_FP"
+else
+    python3 "$BASE_PATH/experiments/research/mevm_style.py" \
+        "$BASE_PATH/experiments/configs/paper_1/mevm_writer_id_no_aug.yaml" \
+        --log_level INFO \
+        --iam_path "$IAM_PATH" \
+        --rimes_path "$RIMES_PATH" \
+        --output_path "$OUT_PATH"/"$DATASPLIT".csv \
+        --mevm_load "$MEVM" \
+        --datasplit "$DATASPLIT" \
+        --embed_filepath "$EMB_FP"
+fi
